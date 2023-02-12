5bfaeec12b454bb780dea3be7d615641e846e14b
alexjwick <alexjwick03@gmail.com>
2023-02-11 21:30:51 -0500

    can get input from repl input box

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+//TODO: replace console.log with creating new element in our history box
+
 // The window.onload callback is invoked when the window is first loaded by the browser
 window.onload = () => {
-  prepareKeypress();
+  prepareTextInput();
   prepareButtonPress();
 
   // If you're adding an event for a button click, do something similar.
@@ -8,43 +10,24 @@ window.onload = () => {
   // should be HTMLButtonElement. The handler function for a "click" takes no arguments.
 };
 
-function prepareKeypress() {
-  // As far as TypeScript knows, there may be *many* elements with this class.
+let replInputBox: HTMLInputElement;
+
+function prepareTextInput() {
   const maybeInputs: HTMLCollectionOf<Element> =
     document.getElementsByClassName("repl-command-box");
-  // Assumption: there's only one thing
   const maybeInput: Element | null = maybeInputs.item(0);
-  // Is the thing there? Is it of the expected type?
-  //  (Remember that the HTML author is free to assign the repl-input class to anything :-) )
   if (maybeInput == null) {
     console.log("Couldn't find input element");
   } else if (!(maybeInput instanceof HTMLInputElement)) {
     console.log(`Found element ${maybeInput}, but it wasn't an input`);
+  } else if (maybeInput.type != "text") {
+    console.log(
+      `Found input element ${maybeInput}, but it wasn't a text input`
+    );
   } else {
-    // Notice that we're passing *THE FUNCTION* as a value, not calling it.
-    // The browser will invoke the function when a key is pressed with the input in focus.
-    //  (This should remind you of the strategy pattern things we've done in Java.)
-    maybeInput.addEventListener("keypress", handleKeypress);
+    replInputBox = maybeInput;
   }
 }
-
-// We'll use a global state reference for now
-let pressCount = 0;
-function getPressCount() {
-  return pressCount;
-}
-
-function handleKeypress(event: KeyboardEvent) {
-  // The event has more fields than just the key pressed (e.g., Alt, Ctrl, etc.)
-  pressCount = pressCount + 1;
-  console.log(
-    `key pressed: ${event.key}. ${getPressCount()} presses seen so far.`
-  );
-}
-
-// Provide this to other modules (e.g., for testing!)
-// The configuration in this project will require /something/ to be exported.
-export { handleKeypress, prepareKeypress, getPressCount };
 
 function prepareButtonPress() {
   const maybeButtons: HTMLCollectionOf<Element> =
@@ -53,12 +36,17 @@ function prepareButtonPress() {
   if (maybeButton == null) {
     console.log("Couldn't find button element");
   } else if (!(maybeButton instanceof HTMLButtonElement)) {
-    console.log(`Foudn element ${maybeButton}, but it wasn't a button`);
+    console.log(`Found element ${maybeButton}, but it wasn't a button`);
   } else {
     maybeButton.addEventListener("click", handleButtonPress);
   }
 }
 
 function handleButtonPress(event: MouseEvent) {
-  alert("Have a great day");
+  //TODO: change this to create/append a text element with this value
+  console.log(replInputBox.value);
+
+  //TODO: handle the command based on this text accordingly
 }
+
+export {};
